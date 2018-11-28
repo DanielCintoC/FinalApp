9f5123dd9876f09b960960170b844921e447a998
Daniel Cinto <control_why_you@hotmail.com>
2018-11-28 17:09:38 -0600

    Login con Google - Log in

--- app/src/main/java/com/fs/dcc/finalapp/LoginActivity.kt
+++ app/src/main/java/com/fs/dcc/finalapp/LoginActivity.kt
@@ -1,19 +1,23 @@
 package com.fs.dcc.finalapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.fs.dcc.finalapp.utils.*
 import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
 
     private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
     private val mGoogleApiClient: GoogleApiClient by lazy { getGoogleApiClient() }
+    private val REQUEST_CODE_GOOGLE_SIGN_IN = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +52,11 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
             editTextPassword.error = if (isValidPassword(it)) null else getString(R.string.error_message_invalid_password)
         }
 
+        buttonLogInGoogle.setOnClickListener {
+            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+            startActivityForResult(signInIntent, REQUEST_CODE_GOOGLE_SIGN_IN)
+        }
+
     }
 
     private fun loginByEmail(email: String, password: String) {
@@ -66,6 +75,13 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         }
     }
 
+    private fun loginByGoogleAccountInToFirebase(googleAccount: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(googleAccount.idToken, null)
+        mAuth.signInWithCredential(credentials).addOnCompleteListener(this) {
+            toast(getString(R.string.message_connection_failed))
+        }
+    }
+
     private fun getGoogleApiClient(): GoogleApiClient {
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -77,6 +93,18 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_GOOGLE_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                val account = result.signInAccount
+                loginByGoogleAccountInToFirebase(account!!)
+            }
+        }
+
     }
 
     override fun onConnectionFailed(connectionResult: ConnectionResult) {
