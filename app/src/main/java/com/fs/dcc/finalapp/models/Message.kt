fb7157f584fd9c661d03d3540cd8884a68281826
Daniel Cinto <control_why_you@hotmail.com>
2018-12-03 15:54:02 -0600

    Layout mensajes chat

---
+++ app/src/main/java/com/fs/dcc/finalapp/models/Message.kt
@@ -0,0 +1,6 @@
+package com.fs.dcc.finalapp.models
+
+import java.util.*
+
+data class Message(val authorId: String, val message: String, val profileImageURL: String,
+                   val sentAt: Date)
