f1ebcc82859854ae07fc91a2ddc67417f2eb032f
Daniel Cinto <control_why_you@hotmail.com>
2018-12-05 14:05:39 -0600

    Implementando InfoFragment

--- app/src/main/java/com/fs/dcc/finalapp/fragments/InfoFragment.kt
+++ app/src/main/java/com/fs/dcc/finalapp/fragments/InfoFragment.kt
@@ -8,18 +8,57 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.fs.dcc.finalapp.R
+import com.fs.dcc.finalapp.utils.CircleTransform
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_info.view.*
 
 class InfoFragment : Fragment() {
 
 
     private lateinit var _view: View
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var currentUser: FirebaseUser
+    private var store: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var chatDBReference: CollectionReference
+    private var chatSubscription: ListenerRegistration? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         _view = inflater.inflate(R.layout.fragment_info, container, false)
+
+        setUpChatDB()
+        setUpCurrentUser()
+        setUpCurrentUserInfoUI()
+
         return _view
 
+    }
+
+    private fun setUpChatDB() {
+        //Va a buscar la colección chat y si no existe la crea, en Firebase
+        chatDBReference = store.collection("chat")
+    }
+
+    private fun setUpCurrentUser() {
+        currentUser = mAuth.currentUser!!
+    }
+
+    private fun setUpCurrentUserInfoUI() {
+        _view.textViewInfoEmail.text = currentUser.email
+        _view.textViewInfoName.text = currentUser.displayName?.let { currentUser.displayName } ?: run {getString(R.string.info_no_name)}
+        currentUser.photoUrl?.let {
+            Picasso.get().load(currentUser.photoUrl).resize(300, 300)
+                    .centerCrop().transform(CircleTransform()).into(_view.imageViewInfoAvatar)
+        } ?: run {
+            Picasso.get().load(R.drawable.ic_avatar).resize(300, 300)
+                    .centerCrop().transform(CircleTransform()).into(_view.imageViewInfoAvatar)
+        }
     }
 
 }
