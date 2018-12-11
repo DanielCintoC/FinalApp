0c081611c7ed4b70076d8631b82a34ab3e6f08d7
Daniel Cinto <control_why_you@hotmail.com>
2018-12-11 16:59:25 -0600

    Implementando Log out

--- app/src/main/java/com/fs/dcc/finalapp/MainActivity.kt
+++ app/src/main/java/com/fs/dcc/finalapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.fs.dcc.finalapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
+import com.fs.dcc.finalapp.activities.LoginActivity
 import com.fs.dcc.finalapp.adapters.PagerAdapter
 import com.fs.dcc.finalapp.fragments.ChatFragment
 import com.fs.dcc.finalapp.fragments.InfoFragment
 import com.fs.dcc.finalapp.fragments.RatesFragment
+import com.fs.dcc.finalapp.utils.goToActivity
 import com.fs.dcc.mylibrary.ToolbarActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ToolbarActivity() {
@@ -81,6 +86,27 @@ class MainActivity : ToolbarActivity() {
 
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.general_options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+
+            R.id.menu_log_out -> {
+                FirebaseAuth.getInstance().signOut()
+                goToActivity<LoginActivity> {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
